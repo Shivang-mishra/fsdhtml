0c418a5556e8ebc5a5bf4e0ea1e7a350f00becb1
Shivang-mishra <mishrashivang819@gmail.com>
2024-12-04 11:45:33 +0530

    intoduction to react

---
+++ jswork/react/index.js
@@ -0,0 +1,26 @@
+const parent =document.getElementById('root');
+console.log(parent);
+console.dir(parent);
+const root=ReactDOM.createRoot(parent);
+//const h2=React.createElement("h2",(style:{color:'cyan'}),"ABES Engineering College")
+
+//const l1=React.createElement("li",{},"Orange");
+//const l2=React.createElement("li",{},"Mango");
+//const ul=React.createElement("ul",{style:{backgroundColor:'cyan'}},{l1,l2});
+
+//root.render(ul);
+
+//JSX
+
+const h2=<h2>Hello world</h2>
+const l1=<li>Orange</li>;
+const l2=<li>Mango</li>;
+const ul=<ul style={{color:'red'}}>{l1}{l2}</ul>
+
+const container=(
+    <div>
+        {h2}
+        <div>{ul}</div>
+    </div>
+)
+root.render(ul);
